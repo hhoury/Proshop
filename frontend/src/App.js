0c1d9dc65f4123b880a6f372f309d07c50055762
hhoury <hhoury@hotmail.com>
2020-12-24 00:16:32 +0200

    header,footer...

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <>
-    <h1>Welcome to Proshop</h1></>
+      <main>
+        <Header></Header>
+        <Container>
+
+          <h1>WELCOME TO PROSHOP</h1>
+
+        </Container>
+        <Footer></Footer>
+      </main>
+    </>
   );
 }
 
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import { Container, Row, Col } from 'react-bootstrap'
+const header = () => {
+    return (
+        <header>
+            Header
+        </header>
+    )
+}
+
+export default header
